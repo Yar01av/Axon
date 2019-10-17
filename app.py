7b01ad99f84c6042aa9466904de924f61b478431
Yar0s1av <yaroslavn99@gmail.com>
2019-10-17 20:58:35 +0200

    Quick configurations added

--- app.py
+++ app.py
@@ -4,16 +4,19 @@ Run this file to use the application!
 
 import gym
 from basic_dqn_agent import DQNAgent
+from env_config import lunar_lander_basic, cart_pole
 from random_agent import RandomAgent
 
 if __name__ == "__main__":
-    env = gym.make('LunarLander-v2')
+    config = lunar_lander_basic
+
+    env = gym.make(config["env_name"])
 
     # Uncomment for DQN agent
-    agent = DQNAgent(8, 4, gym_env=env)
-    agent.train(n_episodes=100)
+    agent = DQNAgent(config["input_dim"], config["output_dim"], gym_env=env)
+    agent.train(n_episodes=50)
     agent.play()
 
     # Uncomment for random agent
-    #agent = RandomAgent(env, 8, 4)
+    #agent = RandomAgent(env, config["input_dim"], config["output_dim"])
     #agent.play()
---
+++ env_config.py
@@ -0,0 +1,9 @@
+# Basic configurations of the possible environments
+
+lunar_lander_basic = {"env_name": 'LunarLander-v2',
+                      "input_dim": 8,
+                      "output_dim": 4}
+
+cart_pole = {"env_name": 'CartPole-v1',
+             "input_dim": 4,
+             "output_dim": 2}
