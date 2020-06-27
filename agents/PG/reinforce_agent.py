60fd72af5304f50787882abd75d18aadbbe2c4e0
Yar0s1av <yaroslavn99@gmail.com>
2020-06-27 13:51:09 +0200

    REINFORCE bug fixed

--- agents/PG/reinforce_agent.py
+++ agents/PG/reinforce_agent.py
@@ -1,22 +1,19 @@
 import numpy as np
 from torch import nn, cuda, FloatTensor, ByteTensor, optim, log_softmax, LongTensor, from_numpy
 
+from agents.PG.util.memory_util import CompositeMemory
 from agents.agent import Agent
 from models.torch_model import TorchModel
 
 
 class REINFORCEAgent(Agent):
     """
-    Agent that follows REINFORCE algorithm. This implementation is build specifically with PyTorch and cannot be easily
+    Agent that follows PG algorithm. This implementation is build specifically with PyTorch and cannot be easily
     switched to Keras.
     """
 
     #TODO:
     # > The model used is used without the wrapper as it was too restrictive. Change the code to make it use the wrapper
-    # > Define the buffer
-    # > Calculate the q-values
-    # > Check if Keras can use loss alone
-    # > Perform SGD
 
     def __init__(self, gym_env, state_size, action_size, n_episodes_to_train=4, learning_rate=0.01, gamma=0.99,
                  model=None):
@@ -25,7 +22,7 @@ class REINFORCEAgent(Agent):
         self._gamma = gamma
         self._lr = learning_rate
         self._n_episodes_to_train = n_episodes_to_train
-        self._memory = _CompositeMemory()
+        self._memory = CompositeMemory()
 
         if model is None:
             self._model = self._build_model()
@@ -63,9 +60,9 @@ class REINFORCEAgent(Agent):
             self._memory.rewards.append(reward)
         else:
             self._memory.q_vals.extend(self._memory.compute_qvals(self._gamma))
-
+ 
     def _learn(self, engine_state):
-        if (engine_state["e"]+1) % self._n_episodes_to_train:
+        if (engine_state["e"]+1) % self._n_episodes_to_train == 0:
             t_act = LongTensor(self._memory.actions).cuda()
             t_state = FloatTensor(np.squeeze(self._memory.states)).cuda()
             t_qval = FloatTensor(self._memory.q_vals).cuda()
@@ -73,6 +70,7 @@ class REINFORCEAgent(Agent):
             self._optimizer.zero_grad()
             t_policy = self._model(t_state)
 
+            # Compute the policy gradient
             loss = -(t_qval*t_policy.log_softmax(dim=1)[range(len(t_state)), t_act]).mean()
 
             loss.backward()
@@ -80,36 +78,3 @@ class REINFORCEAgent(Agent):
 
             # The training step is done. Prepare for the new data.
             self._memory.reset()
-
-        #TODO:
-        # > handle the case of it being time to train or just calculating q vals
-        # > create the model
-
-
-class _CompositeMemory:
-    def __init__(self):
-        self.actions = []
-        self.states = []
-
-        self.q_vals = []
-        self.rewards = []
-
-    # Compute Q-values for the episode
-    def compute_qvals(self, gamma):
-        qs = []
-
-        for reward in self.rewards:
-            if len(qs) == 0:
-                qs.append(reward)
-            else:
-                qs.append(reward + gamma*qs[-1])
-
-        self.rewards.clear()
-
-        return list(reversed(qs))
-
-    def reset(self):
-        self.actions.clear()
-        self.states.clear()
-        self.q_vals.clear()
-        self.rewards.clear()
